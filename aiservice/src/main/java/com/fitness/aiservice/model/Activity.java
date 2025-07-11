e92a54228f39f11c3b8e726fa2f92f49b77f3cfa
banushasarma <sharmabanu03@gmail.com>
2025-07-11 20:54:33 +0530

    RabbitMQ message consume added.

---
+++ aiservice/src/main/java/com/fitness/aiservice/model/Activity.java
@@ -0,0 +1,24 @@
+package com.fitness.aiservice.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+public class Activity {
+
+    private String id;
+    private String userId;
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+    private Map<String, Object> additionalMetrics;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
---
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -0,0 +1,18 @@
+package com.fitness.aiservice.service;
+
+import com.fitness.aiservice.model.Activity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ActivityMessageListener {
+
+    @RabbitListener(queues = "activity.queue")
+    public void processActivity(Activity activity) {
+        log.info("Received activity for processing: {}", activity.getId());
+    }
+}
