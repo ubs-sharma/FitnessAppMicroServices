894b67baab76879df12a85e0187b8d3eafd7bdd1
banushasarma <sharmabanu03@gmail.com>
2025-07-13 13:10:04 +0530

    Recommendation formatted and saved in DB.

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -1,6 +1,8 @@
 package com.fitness.aiservice.service;
 
 import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -12,11 +14,15 @@ import org.springframework.stereotype.Service;
 public class ActivityMessageListener {
 
     private final ActivityAIService activityAIService;
+    private final RecommendationRepository recommendationRepository;
 
     @RabbitListener(queues = "activity.queue")
 
     public void processActivity(Activity activity) {
         log.info("Received activity for processing: {}", activity.getId());
-        log.info("Generated Recommendation: {}", activityAIService.generateRecommendation(activity));
+//        log.info("Generated Recommendation: {}", activityAIService.generateRecommendation(activity));
+        Recommendation recommendation = activityAIService.generateRecommendation(activity);
+        recommendationRepository.save(recommendation);
+        log.info("Recommendation saved: {}", recommendation);
     }
 }
