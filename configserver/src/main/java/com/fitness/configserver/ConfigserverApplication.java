e78e81e7f8ef6be5aec6d671d71216c2aaf7e08a
banushasarma <sharmabanu03@gmail.com>
2025-07-13 13:54:13 +0530

    Config server configuration added.

--- configserver/src/main/java/com/fitness/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/fitness/configserver/ConfigserverApplication.java
@@ -1,9 +1,12 @@
 package com.fitness.configserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
