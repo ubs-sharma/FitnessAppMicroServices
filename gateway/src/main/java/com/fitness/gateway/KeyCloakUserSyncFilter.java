bdb8f22efc8175aa59e626bd75751a745a106355
banushasarma <sharmabanu03@gmail.com>
2025-07-19 12:43:17 +0530

    user extracted from token and validated and saved

---
+++ gateway/src/main/java/com/fitness/gateway/KeyCloakUserSyncFilter.java
@@ -0,0 +1,73 @@
+package com.fitness.gateway;
+
+import com.fitness.gateway.user.RegisterRequest;
+import com.fitness.gateway.user.UserService;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class KeyCloakUserSyncFilter implements WebFilter {
+    private final UserService userService;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String userId = exchange.getRequest().getHeaders().getFirst("X-User-ID");
+        String token = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+        if (userId != null && token != null) {
+            return userService.validateUser(userId)
+                    .flatMap(exist -> {
+                        if (!exist) {
+                            //Register User
+                            RegisterRequest registerRequest = getUserDetails(token);
+                            if (registerRequest != null)
+                                return userService.registerUser(registerRequest)
+                                        .then(Mono.empty());
+                            else
+                                return Mono.empty();
+                        } else {
+                            log.info("User already exists, Skipping sync.");
+                            return Mono.empty();
+                        }
+                    })
+                    .then(Mono.defer(() -> {
+                        ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
+                                .header("X-User-ID", userId)
+                                .build();
+                        return chain.filter(exchange.mutate().request(mutatedRequest).build());
+                    }));
+        }
+        return Mono.empty();
+    }
+
+    private RegisterRequest getUserDetails(String token) {
+        try {
+            String tokenWithoutBearer = token.replace("Bearer", "").trim();
+            SignedJWT signedJWT = SignedJWT.parse(tokenWithoutBearer);
+            JWTClaimsSet claimsSet = signedJWT.getJWTClaimsSet();
+
+            RegisterRequest registerRequest = new RegisterRequest();
+            registerRequest.setEmail(claimsSet.getStringClaim("email"));
+            registerRequest.setKeyCloakId(claimsSet.getStringClaim("sub"));
+            registerRequest.setFirstName(claimsSet.getStringClaim("given_name"));
+            registerRequest.setLastName(claimsSet.getStringClaim("family_name"));
+            registerRequest.setPassword("dummy@123123");
+
+            return registerRequest;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ gateway/src/main/java/com/fitness/gateway/user/UserService.java
@@ -0,0 +1,49 @@
+package com.fitness.gateway.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+    private final WebClient userServiceWebClient;
+
+    public Mono<Boolean> validateUser(String userId) {
+
+        log.info("Calling user validation API for UserId: {}", userId);
+
+        return userServiceWebClient.get().uri("/api/users/{userId}/validate", userId)
+                .retrieve().bodyToMono(Boolean.class)
+                .onErrorResume(WebClientResponseException.class, e -> {
+                    if (e.getStatusCode() == HttpStatus.NOT_FOUND)
+                        throw new RuntimeException("User Not found: " + userId);
+                    else if (e.getStatusCode() == HttpStatus.BAD_REQUEST)
+                        throw new RuntimeException("Invalid request: " + userId);
+                    else throw new RuntimeException("Please try again later: " + userId);
+                });
+    }
+
+    public Mono<UserResponse> registerUser(RegisterRequest registerRequest) {
+
+        log.info("Calling user registration API for email : {}", registerRequest.getEmail());
+
+        return userServiceWebClient.post().uri("/api/users/register")
+                .bodyValue(registerRequest)
+                .retrieve()
+                .bodyToMono(UserResponse.class)
+                .onErrorResume(WebClientResponseException.class, e -> {
+                    if (e.getStatusCode() == HttpStatus.BAD_REQUEST)
+                        throw new RuntimeException("Bad Request: " + e.getMessage());
+                    else if (e.getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR)
+                        throw new RuntimeException("Internal server error: " + e.getMessage());
+                    else
+                        throw new RuntimeException("Unexpected error has occurred. Please try again later: " + e.getMessage());
+                });
+    }
+}
