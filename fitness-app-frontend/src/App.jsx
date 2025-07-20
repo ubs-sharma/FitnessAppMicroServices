4039113d1cc0c2f548da3984de3b9b5ccd7acbc0
banushasarma <sharmabanu03@gmail.com>
2025-07-20 12:08:18 +0530

    Form UI added

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,10 +1,22 @@
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "react-oauth2-code-pkce";
 
 import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router";
 import { setCredentials } from "./store/authSlice";
 import { useDispatch } from "react-redux";
+import ActivityList from "./components/ActivityList";
+import ActivityForm from "./components/ActivityForm";
+import ActivityDetails from "./components/ActivityDetails";
+
+const ActivitiesPage = () => {
+  return (
+    <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
+      <ActivityForm onActityAdded={() => window.location.reload()} />
+      <ActivityList />
+    </Box>
+  );
+};
 
 function App() {
   const { token, tokenData, logIn, logOut, isAuthenticated } =
@@ -32,9 +44,25 @@ function App() {
           LOGIN
         </Button>
       ) : (
-        <div>
-          <pre>{JSON.stringify(tokenData, null, 2)}</pre>
-        </div>
+        <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
+          <Routes>
+            <Route>
+              <Route
+                path="/"
+                element={
+                  token ? (
+                    <Navigate to="/activities" replace />
+                  ) : (
+                    <div>Welcome!. Please log in</div>
+                  )
+                }
+              />
+              <Route path="/activities" element={<ActivitiesPage />} />
+              <Route path="/activities/:id" element={<ActivityDetails />} />
+              <Route path="/activity" element={<ActivityList />} />
+            </Route>
+          </Routes>
+        </Box>
       )}
     </Router>
   );
---
+++ fitness-app-frontend/src/components/ActivityForm.jsx
@@ -0,0 +1,81 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import React, { useState } from "react";
+
+const ActivityForm = (onActivuityAdded) => {
+  const [activity, setActivity] = useState({
+    type: "RUNNING",
+    duration: 0,
+    caloriesBurned: 0,
+    additionalMetrics: {},
+  });
+
+  const [addActivity, setAddActivity] = useState();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // Handle form submission logic here
+    try {
+      await addActivity(activity);
+      onActivuityAdded();
+      setActivity({
+        type: "RUNNUING",
+        duration: 0,
+        caloriesBurned: 0,
+      });
+
+      // Assuming addActivity is a function that handles the form submission
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
+
+  return (
+    <Box component="form" onSubmit={handleSubmit} sx={{ mb: 4 }}>
+      <FormControl fullWidth sx={{ mb: 2 }}>
+        <InputLabel id="activity-type-label">Activity Type</InputLabel>
+        <Select
+          labelId="activity-type-label"
+          value={activity.type}
+          onChange={(e) => setActivity({ ...activity, type: e.target.value })}
+          label="Activity Type"
+        >
+          <MenuItem value="RUNNING">Running</MenuItem>
+          <MenuItem value="CYCLING">Cycling</MenuItem>
+          <MenuItem value="SWIMMING">Swimming</MenuItem>
+          <MenuItem value="WALKING">Walking</MenuItem>
+        </Select>
+      </FormControl>
+      <TextField
+        label="Duration (minutes)"
+        type="number"
+        value={activity.duration}
+        onChange={(e) => setActivity({ ...activity, duration: e.target.value })}
+        fullWidth
+        sx={{ mb: 2 }}
+      />
+      <TextField
+        label="Calories Burned"
+        type="number"
+        value={activity.caloriesBurned}
+        onChange={(e) =>
+          setActivity({ ...activity, caloriesBurned: e.target.value })
+        }
+        fullWidth
+        sx={{ mb: 2 }}
+      />
+      <Button type="submit" variant="contained">
+        Add Activity
+      </Button>
+    </Box>
+  );
+};
+
+export default ActivityForm;
